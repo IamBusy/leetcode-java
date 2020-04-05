3201b98e054bd9cdc99803381e833007990c1ee8
william <1342247033@qq.com>
2020-04-05 16:54:35 +0800

    Use StringBuilder; give another solution

--- src/main/java/com/github/IamBusy/leetcode/_006/Solution.java
+++ src/main/java/com/github/IamBusy/leetcode/_006/Solution.java
@@ -1,10 +1,13 @@
 package com.github.IamBusy.leetcode._006;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public String convert(String s, int numRows) {
         if (numRows == 1) return s;
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<numRows; i++) {
             for (int j = i; j<s.length(); j+=numRows*2 - 2) {
                 sb.append(s.charAt(j));
@@ -14,6 +17,28 @@ public class Solution {
             }
         }
         return sb.toString();
+    }
+
+    public String convert2(String s, int numRows) {
+        if (numRows <= 1) return s;
+        List<StringBuilder> sbs = new ArrayList<StringBuilder>(numRows);
+        for (int i = 0; i< numRows; i++) {
+            sbs.add(new StringBuilder());
+        }
+        int direction = 1;
+        int sbIdx = 0;
+        for (int i = 0; i<s.length(); i++) {
+            sbs.get(sbIdx).append(s.charAt(i));
+            sbIdx += direction;
+            if (sbIdx == 0 || sbIdx == numRows - 1) {
+                direction = 0 - direction;
+            }
+        }
+        for (int i=1; i<numRows; i++) {
+            sbs.get(0).append(sbs.get(i));
+        }
+        return sbs.get(0).toString();
+
     }
 
 }
